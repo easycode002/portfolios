51e05bfcb4d6d939b40e9325a786642c9a1b4e4a
easycode002 <easycode002@gmail.com>
2025-04-27 21:38:08 +0700

    Added: Improve SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://portfolio-three-iota-15.vercel.app",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: "daily",
+  priority: 0.7,
+};
